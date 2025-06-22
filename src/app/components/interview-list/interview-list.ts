98d0ddfab09d3e323d793da36367d2051401eace
Jayraj Sanas <jayrajsanas175@gmail.com>
2025-06-22 20:12:43 +0530

    add: folders and files required for project

---
+++ src/app/components/interview-list/interview-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-list',
+  imports: [],
+  templateUrl: './interview-list.html',
+  styleUrl: './interview-list.scss'
+})
+export class InterviewList {
+
+}
